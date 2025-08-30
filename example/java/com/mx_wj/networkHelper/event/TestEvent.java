610258276a52d7ff7f9d977c73fcf220ba91612b
24718 <2471811489@qq.com>
2025-08-30 19:05:34 +0800

    remove example from project

--- example/java/com/mx_wj/networkHelper/event/TestEvent.java
+++ example/java/com/mx_wj/networkHelper/event/TestEvent.java
@@ -1,19 +1,16 @@
 package com.mx_wj.networkHelper.event;
 
-import com.mx_wj.networkHelper.NetworkHelperMod;
-import com.mx_wj.networkHelper.network.ExamplePacket;
-import com.mx_wj.networkHelper.network.core.PacketManager;
 import net.minecraft.network.chat.PlayerChatMessage;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-//@Mod.EventBusSubscriber
+@Mod.EventBusSubscriber
 public class TestEvent {
-    /*@SubscribeEvent
+    @SubscribeEvent
     public static void on(PlayerInteractEvent.RightClickBlock event) {
         if(!event.getLevel().isClientSide) return;
         PacketManager.sendToServer(NetworkHelperMod.MODID, new ExamplePacket(1, "Hello World!"));
-    }*/
+    }
 }
--- example/java/com/mx_wj/networkHelper/network/ExamplePacket.java
+++ example/java/com/mx_wj/networkHelper/network/ExamplePacket.java
@@ -1,31 +1,39 @@
 package com.mx_wj.networkHelper.network;
 
+import com.mx_wj.networkHelper.network.core.info.IPacket;
 import com.mx_wj.networkHelper.network.core.info.PacketDirection;
 import com.mx_wj.networkHelper.network.core.info.PacketInfo;
-import com.mx_wj.networkHelper.network.easy.EasyPacket;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.network.NetworkEvent;
 
-//@PacketInfo(direction = PacketDirection.CLIENT_TO_SERVER)
-public class ExamplePacket2 extends EasyPacket {
-    private int number;
-    private String message;
+@PacketInfo(direction = PacketDirection.CLIENT_TO_SERVER)
+public class ExamplePacket implements IPacket {
+
+    private final int number;
+    private final String message;
 
     // 发送时使用的构造函数
-    public ExamplePacket2(int number, String message) {
+    public ExamplePacket(int number, String message) {
         this.number = number;
         this.message = message;
     }
 
     // 解码时使用的构造函数 (必须有)
-    public ExamplePacket2(FriendlyByteBuf buf) {
-        super(buf);
+    public ExamplePacket(FriendlyByteBuf buf) {
+        this.number = buf.readInt();
+        this.message = buf.readUtf();
     }
 
     @Override
-    public void process(NetworkEvent.Context context) {
+    public void encode(FriendlyByteBuf buf) {
+        buf.writeInt(number);
+        buf.writeUtf(message);
+    }
+
+    @Override
+    public void execute(NetworkEvent.Context context) {
         ServerPlayer player = context.getSender();
         if (player != null) {
             player.sendSystemMessage(Component.literal("Received from client: " + number + " and '" + message + "'"));
